cc0fd318aee56094c892e6ff294c04747e70f6ef
sadirbaev <user@iMac-apple.local>
2023-02-02 15:26:42 +0500

    Initial commit

---
+++ src/main/java/uz/gilt/usecases/SpringUseCasesApplication.java
@@ -0,0 +1,13 @@
+package uz.gilt.usecases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUseCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUseCasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/gilt/usecases/SpringUseCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.gilt.usecases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUseCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
